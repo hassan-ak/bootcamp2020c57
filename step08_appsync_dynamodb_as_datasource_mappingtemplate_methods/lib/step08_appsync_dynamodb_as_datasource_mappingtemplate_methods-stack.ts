35dc7519257a1bab1328c24e4850347dccd68de1
Hassan Ali Khan <h.a.khan1992fl@gmail.com>
2021-11-03 07:43:49 +0500

    Create AppSync APi

--- step08_appsync_dynamodb_as_datasource_mappingtemplate_methods/lib/step08_appsync_dynamodb_as_datasource_mappingtemplate_methods-stack.ts
+++ step08_appsync_dynamodb_as_datasource_mappingtemplate_methods/lib/step08_appsync_dynamodb_as_datasource_mappingtemplate_methods-stack.ts
@@ -7,5 +7,20 @@ export class Step08AppsyncDynamodbAsDatasourceMappingtemplateMethodsStack extend
     super(scope, id, props);
 
     // The code that defines your stack goes here
+
+    // New appSync API
+    const api = new appsync.GraphqlApi(this, "step08API", {
+      name: "step08NotesApi",
+      schema: appsync.Schema.fromAsset("graphql/schema.gql"),
+      authorizationConfig: {
+        defaultAuthorization: {
+          authorizationType: appsync.AuthorizationType.API_KEY,
+          apiKeyConfig: {
+            expires: cdk.Expiration.after(cdk.Duration.days(20)),
+          },
+        },
+      },
+      xrayEnabled: true,
+    });
   }
 }
