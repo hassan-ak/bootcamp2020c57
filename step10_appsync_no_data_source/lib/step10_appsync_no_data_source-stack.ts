db6a0f7583c7a6556d448e7c9e0223c987cb89b4
Hassan Ali Khan <h.a.khan1992fl@gmail.com>
2021-11-04 08:14:50 +0500

    API

--- step10_appsync_no_data_source/lib/step10_appsync_no_data_source-stack.ts
+++ step10_appsync_no_data_source/lib/step10_appsync_no_data_source-stack.ts
@@ -1,10 +1,19 @@
 import * as cdk from "@aws-cdk/core";
-import * as appsync from "@aws-cdk/aws-appsync";
+import * as appSync from "@aws-cdk/aws-appsync";
 
 export class Step10AppsyncNoDataSourceStack extends cdk.Stack {
   constructor(scope: cdk.Construct, id: string, props?: cdk.StackProps) {
     super(scope, id, props);
 
     // The code that defines your stack goes here
+
+    // AppSync Api
+    const api = new appSync.GraphqlApi(this, "noDataSourceAPI", {
+      name: "noDataSourceAPI",
+      logConfig: {
+        fieldLogLevel: appSync.FieldLogLevel.ALL,
+      },
+      schema: appSync.Schema.fromAsset("graphql/schema.gql"),
+    });
   }
 }
